a0c4d58fa56442aa7b2d29d836e7fcb9d4871281
Dammy <djdamilola01@gmail.com>
2018-10-14 23:32:47 +0100

    create frontend index layout

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Routing\Router;
+/** @var Router $router */
 
-Route::get('/', function () {
-    return view('welcome');
+
+$router->group([
+    'namespace' => 'Front',
+    'as' => 'front.'
+], function (Router $route) {
+
+    $route->get('/', 'HomeController@index')->name('front.index');
+
 });
+
 
 
 
