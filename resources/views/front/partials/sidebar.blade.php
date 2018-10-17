cd53170e7c481e687ca43181879d25930be70732
Dammy <djdamilola01@gmail.com>
2018-10-17 22:57:23 +0100

    fix sidebar memi

--- resources/views/front/partials/sidebar.blade.php
+++ resources/views/front/partials/sidebar.blade.php
@@ -3,81 +3,27 @@
 
 <!-- - -->
 <ul class="menu-sidebar p-t-95 p-b-70">
-    <li class="t-center m-b-13">
-        <a href="index.html" class="txt19">Home</a>
+
+    <li class="t-center m-b-13 {{ Menu::isActiveRoute('front.page.home')  }}">
+        <a class="txt19" href="{{route('front.page.home')}}">Home</a>
     </li>
 
-    <li class="t-center m-b-13">
-        <a href="menu.html" class="txt19">Menu</a>
+    <li class="t-center m-b-13 {{ Menu::isActiveRoute('front.page.menu')  }}">
+        <a class="txt19" href="{{route('front.page.menu')}}">Home</a>
     </li>
 
-    <li class="t-center m-b-13">
-        <a href="gallery.html" class="txt19">Gallery</a>
+    <li class="t-center m-b-13 {{ Menu::isActiveRoute('front.page.about')  }}">
+        <a class="txt19" href="{{route('front.page.about')}}">Home</a>
     </li>
 
-    <li class="t-center m-b-13">
-        <a href="about.html" class="txt19">About</a>
+    <li class="t-center m-b-13 {{ Menu::isActiveRoute('front.page.contact')  }}">
+        <a class="txt19" href="{{route('front.page.contact')}}">Home</a>
     </li>
 
-    <li class="t-center m-b-13">
-        <a href="blog.html" class="txt19">Blog</a>
-    </li>
-
-    <li class="t-center m-b-33">
-        <a href="contact.html" class="txt19">Contact</a>
-    </li>
-
-    <li class="t-center">
+    <li class="t-center {{ Menu::isActiveRoute('front.page.reservation')  }}">
         <!-- Button3 -->
-        <a href="reservation.html" class="btn3 flex-c-m size13 txt11 trans-0-4 m-l-r-auto">
+        <a href="{{route('front.page.reservation')}}" class="btn3 flex-c-m size13 txt11 trans-0-4 m-l-r-auto">
             Reservation
         </a>
     </li>
 </ul>
-
-<!-- - -->
-<div class="gallery-sidebar t-center p-l-60 p-r-60 p-b-40">
-    <!-- - -->
-    <h4 class="txt20 m-b-33">
-        Gallery
-    </h4>
-
-    <!-- Gallery -->
-    <div class="wrap-gallery-sidebar flex-w">
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-01.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-01.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-02.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-02.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-03.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-03.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-05.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-05.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-06.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-06.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-07.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-07.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-09.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-09.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-10.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-10.jpg" alt="GALLERY">
-        </a>
-
-        <a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-11.jpg" data-lightbox="gallery-footer">
-            <img src="images/photo-gallery-thumb-11.jpg" alt="GALLERY">
-        </a>
-    </div>
-</div>
