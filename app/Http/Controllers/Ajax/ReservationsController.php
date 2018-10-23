c354cd74797f7ade84ae77950725373a37a4b85a
Dammy <djdamilola01@gmail.com>
2018-10-23 23:39:51 +0100

    send new reservation notifications to admin

--- app/Http/Controllers/Ajax/ReservationsController.php
+++ app/Http/Controllers/Ajax/ReservationsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Ajax;
 
-use App\Mail\ReservationReceived;
 use App\Models\Reservation;
+use App\Mail\ReservationReceived;
 use App\Http\Controllers\AjaxController;
+use App\Notifications\NewReservation;
 use App\Repositories\ReservationRepository;
 use App\Http\Requests\Front\ReservationRequest;
+use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Mail;
 
 class ReservationsController extends AjaxController
@@ -23,6 +25,9 @@ class ReservationsController extends AjaxController
         $reservation = Reservation::create($request->all());
 
         Mail::to($reservation->email)->send(new ReservationReceived($reservation));
+
+        Notification::route('mail', config('app.email'))
+            ->notify(new NewReservation($reservation));
 
         return $this->sendResponse($reservation, 'Your reservation has been received successfully.');
     }
